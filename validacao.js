45ac716d458b03b7150b8de7055bc5dfb951168c
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-04-17 14:18:36 -0300

    validando CPF

--- validacao.js
+++ validacao.js
@@ -1,3 +1,19 @@
+function valida(input){
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]) {
+        validadores(tipoDeInput)(input)
+    }
+}
+
+
+
+const validadores = {
+    dataNascimento:input => validarDataNascimento(input),
+    cpf:input => validarCPF(input)
+}
+
+
 
 const dataNascimento = document.querySelector('#nascimento')
 
@@ -23,4 +39,37 @@ function maiorQue18(data){
     data.getUTCMoth(), data.getUTCDate())
 
     return dataMais18 <= dataAtual
+}
+
+function validarCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g,'')
+    let mensagem = ''
+    if(!checaCPFRepetido(cpfFormatado)){
+        mensagem = 'O CPF digitado não é válido!'
+    }
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf) {
+            cpfValido = false
+        }
+    })
+
+    return cpfValido
 }
